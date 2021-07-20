2e94ef6bec5506591b0c8b4d13e9691a4a7295a4
Warlock <internalmike@gmail.com>
2021-07-20 07:30:22 +0300

    d

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@
 
 #![deny(warnings)]
 #![doc(test(attr(deny(warnings))))]
+#![doc(test(attr(allow(dead_code))))]
+#![doc(test(attr(allow(unused_variables))))]
+
 #![cfg_attr(all(feature="nightly", feature="std"), feature(const_fn_fn_ptr_basics))]
 #![cfg_attr(feature="nightly", feature(const_fn_trait_bound))]
 //#![cfg_attr(feature="nightly", feature(const_trait_impl))]
