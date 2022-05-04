3afd6f785acf66c6ce5a1cd7d24f1191af2d5f44
Warlock <internalmike@gmail.com>
2022-05-04 15:58:42 +0300

    const trait impls are experimental

---
+++ src/impl_id_nightly.rs
@@ -0,0 +1,9 @@
+impl<C: Component> const ComponentId for Id<C> {
+    fn from_raw(raw: RawId) -> Self {
+        Id { index: raw.0, guard: raw.1, phantom: PhantomType::new() }
+    }
+
+    fn into_raw(self) -> RawId {
+        (self.index, self.guard)
+    }
+}
